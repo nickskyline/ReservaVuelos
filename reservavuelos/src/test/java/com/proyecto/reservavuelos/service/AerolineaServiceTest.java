5998f9ee24a83ce306f93867d42a93e4ab615868
Maria Martinez <mmariayennifer.25@gmail.con>
2023-11-03 08:51:37 -0500

    creando Prueba Unitaria

---
+++ reservavuelos/src/test/java/com/proyecto/reservavuelos/service/AerolineaServiceTest.java
@@ -0,0 +1,17 @@
+package com.proyecto.reservavuelos.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AerolineaServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
